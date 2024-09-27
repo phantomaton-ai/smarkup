2968b2c7fea6748407f69c4c7ad3797e5b399710
phantomaton <182378863+phantomaton-ai@users.noreply.github.com>
2024-09-27 12:57:34 -0700

    Updated by Phantomaton

--- smarkup.test.js
+++ smarkup.test.js
@@ -1,5 +1,5 @@
 import { expect } from 'chai';
-import smarkup from './smarkup.js';
+import smarkup from './smarkup';
 
 describe('smarkup parser', () => {
   it('should parse a simple directive', () => {
@@ -83,6 +83,58 @@ describe('smarkup parser', () => {
         action: 'createProject',
         attributes: {},
         body: ''
+      }
+    ]);
+  });
+
+  it('should handle multi-line, multi-paragraph, multi-directive messages', () => {
+    const input = `/createProject(name:lorem-ipsum)
+    
+Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris eget tempus eros. Fusce vel justo vel magna faucibus pretium. Nullam tempus augue eget nisl euismod, vel efficitur leo tincidunt. Quisque vel risus at eros iaculis bibendum. Morbi id tellus vel magna tincidunt luctus. Aliquam ac elementum velit.
+
+/writeProjectFile(project:lorem-ipsum, file:lorem.txt) {
+Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt.
+}
+
+/createProject(name:ipsum-lorem)
+    
+Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur?
+
+/writeProjectFile(project:ipsum-lorem, file:ipsum.txt) {
+Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?
+}`;
+
+    const directives = smarkup(input);
+    expect(directives).to.deep.equal([
+      {
+        action: 'createProject',
+        attributes: {
+          name: 'lorem-ipsum'
+        },
+        body: ''
+      },
+      {
+        action: 'writeProjectFile',
+        attributes: {
+          project: 'lorem-ipsum',
+          file: 'lorem.txt'
+        },
+        body: 'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt.'
+      },
+      {
+        action: 'createProject',
+        attributes: {
+          name: 'ipsum-lorem'
+        },
+        body: ''
+      },
+      {
+        action: 'writeProjectFile',
+        attributes: {
+          project: 'ipsum-lorem',
+          file: 'ipsum.txt'
+        },
+        body: 'Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?'
       }
     ]);
   });
