5755e037baeaef6935e4616fa7bac654f1e0681c
phantomaton <182378863+phantomaton-ai@users.noreply.github.com>
2024-10-03 09:32:24 -0700

    Updated by Phantomaton

--- smarkup.symbols.js
+++ smarkup.symbols.js
@@ -1,39 +1,22 @@
-const defaultSymbols = {
-  directive: {
-    start: '/',
-    end: '!'
-  },
-  attributes: {
-    start: '(',
-    separator: ',',
-    end: ')'
-  },
-  pair: {
-    separator: ':'
-  },
-  body: {
-    start: '{',
-    end: '}'
-  }
-};
-
-export const getSymbols = (options = {}) => {
+const symbols = (options = {}) => {
   return {
     directive: {
-      start: options?.directive?.start || defaultSymbols.directive.start,
-      end: options?.directive?.end || defaultSymbols.directive.end
+      start: options?.directive?.start || '/',
+      end: options?.directive?.end || '!'
     },
     attributes: {
-      start: options?.attributes?.start || defaultSymbols.attributes.start,
-      separator: options?.attributes?.separator || defaultSymbols.attributes.separator,
-      end: options?.attributes?.end || defaultSymbols.attributes.end
+      start: options?.attributes?.start || '(',
+      separator: options?.attributes?.separator || ',',
+      end: options?.attributes?.end || ')'
     },
     pair: {
-      separator: options?.pair?.separator || defaultSymbols.pair.separator
+      separator: options?.pair?.separator || ':'
     },
     body: {
-      start: options?.body?.start || defaultSymbols.body.start,
-      end: options?.body?.end || defaultSymbols.body.end
+      start: options?.body?.start || '{',
+      end: options?.body?.end || '}'
     }
   };
 };
+
+export default symbols;
