57953c0f4ebf899910b219ef9abd33407d943b7d
phantomaton <182378863+phantomaton-ai@users.noreply.github.com>
2024-10-03 10:28:58 -0700

    Updated by Phantomaton

--- smarkup.symbols.test.js
+++ smarkup.symbols.test.js
@@ -1,63 +1,65 @@
 import { expect } from 'chai';
-import symbols from './smarkup.symbols.js';
+import { getSymbols } from './smarkup.symbols.js';
 
 describe('Smarkup Symbols', () => {
-  it('should use default symbols', () => {
-    const symbolConfig = symbols();
-    expect(symbolConfig.directive.start).to.equal('/');
-    expect(symbolConfig.directive.end).to.equal('!');
-    expect(symbolConfig.attributes.start).to.equal('(');
-    expect(symbolConfig.attributes.separator).to.equal(',');
-    expect(symbolConfig.attributes.end).to.equal(')');
-    expect(symbolConfig.pair.separator).to.equal(':');
-    expect(symbolConfig.body.start).to.equal('{');
-    expect(symbolConfig.body.end).to.equal('}');
-  });
+  describe('getSymbols', () => {
+    it('should use default symbols', () => {
+      const symbolConfig = getSymbols();
+      expect(symbolConfig.directive.start).to.equal('/');
+      expect(symbolConfig.directive.end).to.equal('!');
+      expect(symbolConfig.attributes.start).to.equal('(');
+      expect(symbolConfig.attributes.separator).to.equal(',');
+      expect(symbolConfig.attributes.end).to.equal(')');
+      expect(symbolConfig.pair.separator).to.equal(':');
+      expect(symbolConfig.body.start).to.equal('{');
+      expect(symbolConfig.body.end).to.equal('}');
+    });
 
-  it('should override default symbols', () => {
-    const options = {
-      directive: {
-        start: '🪄✨ ',
-        end: '⚡️'
-      },
-      attributes: {
-        start: '✨🌟⭐️',
-        separator: '✨💫✨',
-        end: '⭐️🌟✨'
-      },
-      pair: {
-        separator: ' 🔮 '
-      },
-      body: {
-        start: '✨📜',
-        end: '📜✨'
-      }
-    };
-    const symbolConfig = symbols(options);
-    expect(symbolConfig.directive.start).to.equal('🪄✨ ');
-    expect(symbolConfig.directive.end).to.equal('⚡️');
-    expect(symbolConfig.attributes.start).to.equal('✨🌟⭐️');
-    expect(symbolConfig.attributes.separator).to.equal('✨💫✨');
-    expect(symbolConfig.attributes.end).to.equal('⭐️🌟✨');
-    expect(symbolConfig.pair.separator).to.equal(' 🔮 ');
-    expect(symbolConfig.body.start).to.equal('✨📜');
-    expect(symbolConfig.body.end).to.equal('📜✨');
-  });
+    it('should override default symbols', () => {
+      const options = {
+        directive: {
+          start: '🪄✨ ',
+          end: '⚡️'
+        },
+        attributes: {
+          start: '✨🌟⭐️',
+          separator: '✨💫✨',
+          end: '⭐️🌟✨'
+        },
+        pair: {
+          separator: ' 🔮 '
+        },
+        body: {
+          start: '✨📜',
+          end: '📜✨'
+        }
+      };
+      const symbolConfig = getSymbols(options);
+      expect(symbolConfig.directive.start).to.equal('🪄✨ ');
+      expect(symbolConfig.directive.end).to.equal('⚡️');
+      expect(symbolConfig.attributes.start).to.equal('✨🌟⭐️');
+      expect(symbolConfig.attributes.separator).to.equal('✨💫✨');
+      expect(symbolConfig.attributes.end).to.equal('⭐️🌟✨');
+      expect(symbolConfig.pair.separator).to.equal(' 🔮 ');
+      expect(symbolConfig.body.start).to.equal('✨📜');
+      expect(symbolConfig.body.end).to.equal('📜✨');
+    });
 
-  it('should use default for missing options', () => {
-    const options = {
-      directive: {
-        start: '🪄✨ '
-      }
-    };
-    const symbolConfig = symbols(options);
-    expect(symbolConfig.directive.start).to.equal('🪄✨ ');
-    expect(symbolConfig.directive.end).to.equal('!');
-    expect(symbolConfig.attributes.start).to.equal('(');
-    expect(symbolConfig.attributes.separator).to.equal(',');
-    expect(symbolConfig.attributes.end).to.equal(')');
-    expect(symbolConfig.pair.separator).to.equal(':');
-    expect(symbolConfig.body.start).to.equal('{');
-    expect(symbolConfig.body.end).to.equal('}');
+    it('should use default for missing options', () => {
+      const options = {
+        directive: {
+          start: '🪄✨ '
+        }
+      };
+      const symbolConfig = getSymbols(options);
+      expect(symbolConfig.directive.start).to.equal('🪄✨ ');
+      expect(symbolConfig.directive.end).to.equal('!');
+      expect(symbolConfig.attributes.start).to.equal('(');
+      expect(symbolConfig.attributes.separator).to.equal(',');
+      expect(symbolConfig.attributes.end).to.equal(')');
+      expect(symbolConfig.pair.separator).to.equal(':');
+      expect(symbolConfig.body.start).to.equal('{');
+      expect(symbolConfig.body.end).to.equal('}');
+    });
   });
 });
