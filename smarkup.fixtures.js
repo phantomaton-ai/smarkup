3c250df296c01efd10d17377e8b99596698a92e2
phantomaton <182378863+phantomaton-ai@users.noreply.github.com>
2024-10-03 11:34:56 -0700

    more

--- smarkup.fixtures.js
+++ smarkup.fixtures.js
@@ -9,7 +9,7 @@ export const simple = fixture(
 );
 
 export const body = fixture(
-  '/writeProjectFile(project:smarkup, file:example.txt) {\nThis is the content.\n} writeProjectFile!',
+  '/writeProjectFile(project:smarkup,file:example.txt) {\nThis is the content.\n} writeProjectFile!',
   [{
     action: 'writeProjectFile',
     attributes: {
@@ -21,7 +21,7 @@ export const body = fixture(
 );
 
 export const multiple = fixture(
-  '/createProject(name:test)\n/writeProjectFile(project:test, file:example.txt) {\nThis is the content.\n} writeProjectFile!',
+  '/createProject(name:test)\n/writeProjectFile(project:test,file:example.txt) {\nThis is the content.\n} writeProjectFile!',
   [
     {
       action: 'createProject',
@@ -95,6 +95,31 @@ Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium dolor
 );
 
 export const customized = fixture(
+  `🪄✨ createProject✨🌟⭐️name 🔮 lorem-ipsum⭐️🌟✨
+🪄✨ writeProjectFile✨🌟⭐️project 🔮 lorem-ipsum✨💫✨file 🔮 lorem.txt⭐️🌟✨ ✨📜
+Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt.
+📜✨ writeProjectFile⚡️`,
+  [
+    {
+      action: 'createProject',
+      attributes: {
+        name: 'lorem-ipsum'
+      },
+      body: undefined
+    },
+    {
+      action: 'writeProjectFile',
+      attributes: {
+        project: 'lorem-ipsum',
+        file: 'lorem.txt'
+      },
+      body: 'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt.'
+    }
+  ],
+  customs
+);
+
+export const challenging = fixture(
   `🪄✨ createProject ✨🌟⭐️ name 🔮 lorem-ipsum ⭐️🌟✨
     
 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris eget tempus eros. Fusce vel justo vel magna faucibus pretium. Nullam tempus augue eget nisl euismod, vel efficitur leo tincidunt. Quisque vel risus at eros iaculis bibendum. Morbi id tellus vel magna tincidunt luctus. Aliquam ac elementum velit.
--- smarkup.render.test.js
+++ smarkup.render.test.js
@@ -2,17 +2,13 @@ import { expect } from 'chai';
 import render from './smarkup.render.js';
 import { simple, body, multiple, customized } from './smarkup.fixtures.js';
 
-const like = ({ directives, symbols, expected }) => () => {
-  const { text } = render(directives, symbols);
-  expect(text).to.equal(expected);
+const like = ({ directives, symbols, text }) => () => {
+  expect(render(directives, symbols)).to.equal(text);
 };
 
-describe('Smarkup renderer', () => {
+describe('smarkup.render', () => {
   it('renders a simple directive', like(simple));
-
   it('renders a directive with a body', like(body));
-
   it('renders multiple directives', like(multiple));
-
   it('handles custom symbols', like(customized));
 });
