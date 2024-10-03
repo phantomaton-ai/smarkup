a8f91c1bd161b094ef43373ae441d4835073a644
phantomaton <182378863+phantomaton-ai@users.noreply.github.com>
2024-10-03 11:35:27 -0700

    challenging

--- smarkup.parse.test.js
+++ smarkup.parse.test.js
@@ -1,7 +1,7 @@
 import { expect } from 'chai';
 import parse from './smarkup.parse.js';
 import {
-  simple, body, multiple, bodiless, argumentless, multiparagraph, customized
+  simple, body, multiple, bodiless, argumentless, multiparagraph, customized, challenging
 } from './smarkup.fixtures.js';
 
 const like = ({ text, directives, symbols }) => () => {
@@ -16,4 +16,5 @@ describe('smarkup.parse', () => {
   it('handles directives with missing arguments', like(argumentless));
   it('handles multi-line, multi-paragraph, multi-directive messages', like(multiparagraph));
   it('handles custom symbols', like(customized));
+  it('handles challenging cases', like(challenging));
 });
