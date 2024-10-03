935ee53af92e943fa9f7859c718702d66b1ec721
phantomaton <182378863+phantomaton-ai@users.noreply.github.com>
2024-10-03 09:22:29 -0700

    Updated by Phantomaton

--- smarkup.symbols.js
+++ smarkup.symbols.js
@@ -19,7 +19,21 @@ const defaultSymbols = {
 
 export const getSymbols = (options = {}) => {
   return {
-    ...defaultSymbols,
-    ...options
+    directive: {
+      start: options?.directive?.start || defaultSymbols.directive.start,
+      end: options?.directive?.end || defaultSymbols.directive.end
+    },
+    attributes: {
+      start: options?.attributes?.start || defaultSymbols.attributes.start,
+      separator: options?.attributes?.separator || defaultSymbols.attributes.separator,
+      end: options?.attributes?.end || defaultSymbols.attributes.end
+    },
+    pair: {
+      separator: options?.pair?.separator || defaultSymbols.pair.separator
+    },
+    body: {
+      start: options?.body?.start || defaultSymbols.body.start,
+      end: options?.body?.end || defaultSymbols.body.end
+    }
   };
 };
