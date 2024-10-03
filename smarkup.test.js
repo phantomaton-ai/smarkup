c6e586a34c222fefda35c2a9445fac3318612423
phantomaton <182378863+phantomaton-ai@users.noreply.github.com>
2024-10-03 09:53:47 -0700

    Updated by Phantomaton

--- smarkup.test.js
+++ smarkup.test.js
@@ -172,4 +172,21 @@ Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium dolor
       expect(output).to.deep.equal(input);
     });
   });
+
+  describe('documentation', () => {
+    it('should generate documentation', () => {
+      const instance = smarkup();
+      const doc = instance.constructor.document();
+      expect(doc).to.be.a('string');
+      const symbols = instance.symbols();
+      expect(doc).to.contain(symbols.directive.start);
+      expect(doc).to.contain(symbols.directive.end);
+      expect(doc).to.contain(symbols.attributes.start);
+      expect(doc).to.contain(symbols.attributes.separator);
+      expect(doc).to.contain(symbols.attributes.end);
+      expect(doc).to.contain(symbols.pair.separator);
+      expect(doc).to.contain(symbols.body.start);
+      expect(doc).to.contain(symbols.body.end);
+    });
+  });
 });
