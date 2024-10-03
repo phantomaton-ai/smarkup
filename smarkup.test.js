7df86d2483a73ea35de629ef990a1a8e9b3dd4aa
phantomaton <182378863+phantomaton-ai@users.noreply.github.com>
2024-10-03 10:01:21 -0700

    Updated by Phantomaton

--- smarkup.test.js
+++ smarkup.test.js
@@ -176,7 +176,42 @@ Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium dolor
   describe('documentation', () => {
     it('generates documentation', () => {
       const instance = smarkup();
-      const doc = instance.constructor.document();
+      const doc = instance.document();
+      expect(doc).to.be.a('string');
+      const symbols = instance.symbols();
+      expect(doc).to.contain(symbols.directive.start);
+      expect(doc).to.contain(symbols.directive.end);
+      expect(doc).to.contain(symbols.attributes.start);
+      expect(doc).to.contain(symbols.attributes.separator);
+      expect(doc).to.contain(symbols.attributes.end);
+      expect(doc).to.contain(symbols.pair.separator);
+      expect(doc).to.contain(symbols.body.start);
+      expect(doc).to.contain(symbols.body.end);
+    });
+
+    it('generates documentation with custom symbols', () => {
+      const options = {
+        symbols: {
+          directive: {
+            start: '🪄✨ ',
+            end: '⚡️'
+          },
+          attributes: {
+            start: '✨🌟⭐️',
+            separator: '✨💫✨',
+            end: '⭐️🌟✨'
+          },
+          pair: {
+            separator: ' 🔮 '
+          },
+          body: {
+            start: '✨📜',
+            end: '📜✨'
+          }
+        }
+      };
+      const instance = smarkup(options);
+      const doc = instance.document();
       expect(doc).to.be.a('string');
       const symbols = instance.symbols();
       expect(doc).to.contain(symbols.directive.start);
