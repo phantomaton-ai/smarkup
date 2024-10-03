530d15c0d429991590c0021ff040107b057475d0
phantomaton <182378863+phantomaton-ai@users.noreply.github.com>
2024-10-03 11:16:11 -0700

    Updated by Phantomaton

--- smarkup.fixtures.js
+++ smarkup.fixtures.js
@@ -11,16 +11,16 @@ export const simple = fixture(
 export const body = fixture(
   '/writeProjectFile(project:smarkup, file:example.txt) {\nThis is the content.\n} writeProjectFile!',
   [{
-      action: 'writeProjectFile',
-      attributes: {
-        project: 'smarkup',
-        file: 'example.txt'
-      },
-      body: 'This is the content.'
+    action: 'writeProjectFile',
+    attributes: {
+      project: 'smarkup',
+      file: 'example.txt'
+    },
+    body: 'This is the content.'
   }]
 );
 
-export const multiple = fixture(
+export const multipleDirectives = fixture(
   '/createProject(name:test)\n/writeProjectFile(project:test, file:example.txt) {\nThis is the content.\n} writeProjectFile!',
   [
     {
@@ -41,4 +41,57 @@ export const multiple = fixture(
   ]
 );
 
+export const multilineMultiParagraph = fixture(
+  `/createProject(name:lorem-ipsum)
+    
+Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris eget tempus eros. Fusce vel justo vel magna faucibus pretium. Nullam tempus augue eget nisl euismod, vel efficitur leo tincidunt. Quisque vel risus at eros iaculis bibendum. Morbi id tellus vel magna tincidunt luctus. Aliquam ac elementum velit.
 
+/writeProjectFile(project:lorem-ipsum, file:lorem.txt) {
+Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt.
+} writeProjectFile!`,
+  [
+    {
+      action: 'createProject',
+      attributes: {
+        name: 'lorem-ipsum'
+      },
+      body: undefined
+    },
+    {
+      action: 'writeProjectFile',
+      attributes: {
+        project: 'lorem-ipsum',
+        file: 'lorem.txt'
+      },
+      body: 'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt.'
+    }
+  ]
+);
+
+export const customSymbols = fixture(
+  `🪄✨ createProject ✨🌟⭐️ name 🔮 lorem-ipsum ⭐️🌟✨
+    
+Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris eget tempus eros. Fusce vel justo vel magna faucibus pretium. Nullam tempus augue eget nisl euismod, vel efficitur leo tincidunt. Quisque vel risus at eros iaculis bibendum. Morbi id tellus vel magna tincidunt luctus. Aliquam ac elementum velit.
+
+🪄✨ writeProjectFile  ✨🌟⭐️ project 🔮 lorem-ipsum ✨💫✨ file 🔮 lorem.txt ⭐️🌟✨ ✨📜
+Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt.
+📜✨ writeProjectFile ⚡️`,
+  [
+    {
+      action: 'createProject',
+      attributes: {
+        name: 'lorem-ipsum'
+      },
+      body: undefined
+    },
+    {
+      action: 'writeProjectFile',
+      attributes: {
+        project: 'lorem-ipsum',
+        file: 'lorem.txt'
+      },
+      body: 'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt.'
+    }
+  ],
+  customs
+);
